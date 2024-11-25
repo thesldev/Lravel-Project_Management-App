0469e59c7b0d919d95e74ab859d1237c27af012a
S.M.D.Sanka Siriwardhana <siriwardhanad.sanka@gmail.com>
2024-11-25 13:33:44 +0530

    Create 3 seperate routes for 3 roals  Changes to be committed: 	modified:   app/Http/Controllers/Auth/AuthenticatedSessionController.php 	new file:   resources/views/adminDashboard.blade.php 	new file:   resources/views/superAdmindashboard.blade.php

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,16 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+Route::get('/admin/dashboard', function () {
+    return view('adminDashboard');
+})->middleware(['auth', 'verified'])->name('adminDashboard');
+
+
+Route::get('/sup-admin/dashboard', function () {
+    return view('superAdmindashboard');
+})->middleware(['auth', 'verified'])->name('supAdminDashboard');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
