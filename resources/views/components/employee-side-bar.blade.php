d1affe7b1848eed464a3c100fe337c6b24f279cd
S.M.D.Sanka Siriwardhana <siriwardhanad.sanka@gmail.com>
2024-11-28 12:31:28 +0530

    create seperate side bar for employees  Changes to be committed: 	new file:   resources/views/components/employee-side-bar.blade.php
     Changes not staged for commit:
    	modified:   resources/views/employeeDashboard.blade.php

---
+++ resources/views/components/employee-side-bar.blade.php
@@ -0,0 +1,177 @@
+<ul
+    class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion"
+     id="accordionSidebar"
+    >
+    <!-- Sidebar - Brand -->
+    <a
+        class="sidebar-brand d-flex align-items-center justify-content-center"
+        href={{ route('employeeDashboard') }}
+    >
+        <div class="sidebar-brand-text mx-3">Claps Dev</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0" />
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="{{ route('employeeDashboard') }}">
+            <i class="fas fa-fw fa-chart-line"></i>
+            <span>Dashboard</span>
+        </a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider" />
+
+    <!-- Heading -->
+    <div class="sidebar-heading">Interface</div>
+
+    <!-- Nav Item - clients Collapse Menu -->
+    <li class="nav-item">
+        <a 
+            class="nav-link collapsed" 
+            href="#" 
+            data-toggle="collapse" 
+            data-target="#collapseClients" 
+            aria-expanded="true" 
+            aria-controls="collapseClients"
+        >
+            <i class="fas fa-fw fa-users"></i>
+            <span>Clients</span>
+        </a>
+        <div 
+            id="collapseClients" 
+            class="collapse" 
+            aria-labelledby="headingClients" 
+            data-parent="#accordionSidebar"
+        >
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Client Options:</h6>
+                <a class="collapse-item" href="{{ route('client.index') }}">View All Clients</a>
+                <a class="collapse-item" href="{{ route('client.create') }}">Add New Client</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - projects Collapse Menu -->
+    <li class="nav-item">
+        <a
+        class="nav-link collapsed"
+        href="#"
+        data-toggle="collapse"
+        data-target="#collapseUtilities"
+        aria-expanded="true"
+        aria-controls="collapseUtilities"
+        >
+        <i class="fas fa-clipboard-list"></i>
+        <span>Projects</span>
+        </a>
+        <div
+        id="collapseUtilities"
+        class="collapse"
+        aria-labelledby="headingUtilities"
+        data-parent="#accordionSidebar"
+        >
+        <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Manage Projects:</h6>
+            <a class="collapse-item" href="{{route('projects.index')}}">View All Projects</a>
+            <a class="collapse-item" href="{{route('project.create')}}">Add New Project</a>
+        </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - Employees Collapse Menu -->
+    <li class="nav-item">
+        <a
+            class="nav-link collapsed"
+            href="#"
+            data-toggle="collapse"
+            data-target="#collapseEmployees"
+            aria-expanded="true"
+            aria-controls="collapseEmployees"
+        >
+            <i class="fas fa-user-friends"></i>
+            <span>Employees</span>
+        </a>
+        <div
+            id="collapseEmployees"
+            class="collapse"
+            aria-labelledby="headingEmployees"
+            data-parent="#accordionSidebar"
+        >
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Manage Employees:</h6>
+                <a class="collapse-item" href="{{route('employee.index')}}">View All Employees</a>
+                <a class="collapse-item" href="{{route('employee.create')}}">Add New Employee</a>
+            </div>
+        </div>
+    </li>
+
+
+    <!-- Divider -->
+    <hr class="sidebar-divider" />
+
+    <!-- Heading -->
+    <div class="sidebar-heading">Addons</div>
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a
+        class="nav-link collapsed"
+        href="#"
+        data-toggle="collapse"
+        data-target="#collapsePages"
+        aria-expanded="true"
+        aria-controls="collapsePages"
+        >
+        <i class="fas fa-fw fa-folder"></i>
+        <span>Pages</span>
+        </a>
+        <div
+        id="collapsePages"
+        class="collapse"
+        aria-labelledby="headingPages"
+        data-parent="#accordionSidebar"
+        >
+        <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Login Screens:</h6>
+            <a class="collapse-item" href="login.html">Login</a>
+            <a class="collapse-item" href="register.html">Register</a>
+            <a class="collapse-item" href="forgot-password.html"
+            >Forgot Password</a
+            >
+            <div class="collapse-divider"></div>
+            <h6 class="collapse-header">Other Pages:</h6>
+            <a class="collapse-item" href="404.html">404 Page</a>
+            <a class="collapse-item" href="blank.html">Blank Page</a>
+        </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - Charts -->
+    <li class="nav-item">
+        <a class="nav-link" href="charts.html">
+        <i class="fas fa-fw fa-chart-area"></i>
+        <span>Charts</span></a
+        >
+    </li>
+
+    <!-- Nav Item - Tables -->
+    <li class="nav-item">
+        <a class="nav-link" href="tables.html">
+        <i class="fas fa-fw fa-table"></i>
+        <span>Tables</span></a
+        >
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block" />
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+    </ul>
+
+    <x-logoutModule />
